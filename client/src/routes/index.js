42fe37f17ba7e38eb395ffff5eec3512f7c52d99
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-12-08 23:59:29 -0800

    add viwer context

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -5,18 +5,48 @@ import Items from './../pages/Items'
 import Share from './../pages/Share'
 import Profile from './../pages/Profile'
 import NavBar from './../components/Header'
+import { ViewerContext } from '../context/ViewerProvider'
+
 
 export default () => (
-  <Fragment>
-    <NavBar />
-    <Switch>
-      <Route exact path="/items" component={Items} />
-      <Route exact path="/welcome" component={Home} />
-      <Route exact path="/share" component={Share} />
-      <Route exact path="/profile" component={Profile} />
-      <Route exact path="/profile/:id" component={Profile} />
-      <Redirect from="/" exact to="/welcome" component={Home} />
-      <Redirect to="/items" component={Items} />
-    </Switch>
-  </Fragment>
+  <ViewerContext.Consumer>
+    {({ loading, viewer, error }) => {
+      if (loading) return 'Loading stuff'
+      if (!viewer) {
+        return (
+          <Switch>
+            <Route
+              exact
+              path='/welcome'
+              name='/home'
+              component={Home}
+            // THIS PART IS CALLED LOADABLE CODE SPLITTING - ADVANCED STUFF. 
+            // component={Loadable({
+            //   loader: () => import('../Pages/HomePage'),
+            //   loading: FullScreenLoader
+            // })}
+            />
+            <Redirect from='*' to='/welcome' />
+          </Switch>
+        )
+      } else {
+        return (
+          <Fragment>
+            <NavBar />
+            <Switch>
+              <Route exact path="/items" component={Items} />
+              <Route exact path="/share" component={Share} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/profile/:id" component={Profile} />
+              <Redirect to="/items" component={Items} />
+            </Switch>
+          </Fragment>
+
+        )
+
+
+      }
+    }}
+  </ViewerContext.Consumer>
+
 );
